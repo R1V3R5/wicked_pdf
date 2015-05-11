8d6c5dd8fa0b11fe1bde30fc08e4b1d655ed15a6
Joshua Rivers <joshua.rivers@gmail.com>
2015-05-10 22:42:12 -0400

    updated

--- lib/wicked_pdf/railtie.rb
+++ lib/wicked_pdf/railtie.rb
@@ -1,16 +1,12 @@
-require 'wicked_pdf/pdf_helper'
-require 'wicked_pdf/wicked_pdf_helper'
+class WickedPdfRailtie < Rails::Railtie
 
-class WickedRailtie < Rails::Railtie
-  initializer 'wicked_pdf.register' do |app|
+  initializer 'wicked_pdf.configure_rails_initialization' do
     ActionController::Base.send :include, PdfHelper
     ActionView::Base.send :include, WickedPdfHelper::Assets
+    if Mime::Type.lookup_by_extension(:pdf).nil?
+      Mime::Type.register('application/pdf', :pdf)
+    end
   end
 end
-
-
-
-
-if Mime::Type.lookup_by_extension(:pdf).nil?
-  Mime::Type.register('application/pdf', :pdf)
-end
+require 'wicked_pdf/pdf_helper'
+require 'wicked_pdf/wicked_pdf_helper'
